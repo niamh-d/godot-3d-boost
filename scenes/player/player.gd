a307baf87994f2fcc135f42d96bbbd0df6ca5ac3
Niamh <niamh.linguist@gmail.com>
2025-02-09 20:03:16 +0200

    level scene

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,3 +3,7 @@ extends Node3D
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("space"):
 		position.y += delta
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
